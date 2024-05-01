5ccbb891816ba4dba5809eb0761815f0467d4563
alpklc <54235090+alpklc@users.noreply.github.com>
2024-05-01 20:35:41 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,16 @@
+package com.alp.main;
+
+import com.alp.ui.MainScreen;
+
+public class App {
+
+	/**
+	 * @param args
+	 * This is main method that's entry point of application.
+	 */
+	public static void main(String[] args) {
+		
+		new MainScreen();
+	}
+
+}
